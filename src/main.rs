5928e7a3fb1a4975a750793e9e959506e15dad8c
ujjwalraj123 <ujjwalr559@gmail.com>
2026-03-26 08:23:24 -0400

    made a helloWorld API

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,38 @@
 use dotenvy::dotenv;
 use sqlx::postgres::PgPoolOptions;
+ use axum::{
+    routing::get,
+    Router,
+};
+use tracing::{info, Level};
+use tracing_subscriber;
  
 #[tokio::main]
 async fn main() -> Result<(), sqlx::Error> {
+    // initialize tracing for logging
+    tracing_subscriber::fmt()
+        .with_max_level(Level::INFO)
+        .init();
+ 
     dotenv().ok();
     let url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     let pool = PgPoolOptions::new().connect(&url).await?;
-    println!("Connected to the database!");
+    info!("Connected to the database!");
+ 
+    // build our application with a route
+    let app = Router::new()
+        // `GET /` goes to `root`
+        .route("/", get(root));
+ 
+    // run our app with hyper, listening globally on port 5000
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:5000").await.unwrap();
+    info!("Server is running on http://0.0.0.0:5000");
+    axum::serve(listener, app).await.unwrap();
  
     Ok(())
+}
+ 
+// handler for GET /
+async fn root() -> &'static str {
+    "Hello, world!"
 }
