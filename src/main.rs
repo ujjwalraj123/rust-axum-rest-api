7e4968436ef16bfe46c5e722a6af83355a107ce4
ujjwalraj123 <ujjwalr559@gmail.com>
2026-03-26 08:07:23 -0400

    Connected to The Database

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,12 @@
+use dotenvy::dotenv;
+use sqlx::postgres::PgPoolOptions;
+ 
 #[tokio::main]
-async fn main() {
-
+async fn main() -> Result<(), sqlx::Error> {
+    dotenv().ok();
+    let url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let pool = PgPoolOptions::new().connect(&url).await?;
+    println!("Connected to the database!");
+ 
+    Ok(())
 }
