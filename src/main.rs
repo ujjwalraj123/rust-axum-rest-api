2d6b5c6dd85fd56148afcc32044e985fb53ff78f
ujjwalraj123 <ujjwalr559@gmail.com>
2026-03-26 08:55:35 -0400

    Added Extension Layer

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,35 @@
+use axum::{Router, routing::get};
 use dotenvy::dotenv;
 use sqlx::postgres::PgPoolOptions;
- use axum::{
-    routing::get,
-    Router,
-};
-use tracing::{info, Level};
-use tracing_subscriber;
- 
+use tracing::{Level, info};
+use tracing_subscriber::{self, fmt::layer};
+use axum::{extract::Extension, routing::get, Json, Router};
+
 #[tokio::main]
 async fn main() -> Result<(), sqlx::Error> {
     // initialize tracing for logging
-    tracing_subscriber::fmt()
-        .with_max_level(Level::INFO)
-        .init();
- 
+    tracing_subscriber::fmt().with_max_level(Level::INFO).init();
+    
     dotenv().ok();
     let url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     let pool = PgPoolOptions::new().connect(&url).await?;
     info!("Connected to the database!");
- 
+    
     // build our application with a route
     let app = Router::new()
-        // `GET /` goes to `root`
-        .route("/", get(root));
- 
-    // run our app with hyper, listening globally on port 5000
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:5000").await.unwrap();
-    info!("Server is running on http://0.0.0.0:5000");
-    axum::serve(listener, app).await.unwrap();
- 
-    Ok(())
+    // `GET /` goes to `root`
+    .route("/", get(root))
+    //Extension Layer
+    .layer(Extension(pool));
+
+// run our app with hyper, listening globally on port 5000
+let listener = tokio::net::TcpListener::bind("0.0.0.0:5000").await.unwrap();
+info!("Server is running on http://0.0.0.0:5000");
+axum::serve(listener, app).await.unwrap();
+
+Ok(())
 }
- 
+
 // handler for GET /
 async fn root() -> &'static str {
     "Hello, world!"
