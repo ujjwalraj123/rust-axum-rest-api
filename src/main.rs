b2cde0338a5863b1c8b99da98874efc1146cdafc
ujjwalraj123 <ujjwalr559@gmail.com>
2026-03-27 09:37:30 -0400

    Started Following a github repo for rust rest api

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,48 @@
-use axum::{Router, routing::get};
+use std::sync::Arc;
+
+use axum::http::{
+    header::{ACCEPT, AUTHORIZATION, CONTENT_TYPE},
+    HeaderValue, Method,
+};
 use dotenvy::dotenv;
-use sqlx::postgres::PgPoolOptions;
-use tracing::{Level, info};
-use tracing_subscriber::{self, fmt::layer};
-use axum::{extract::Extension, routing::get, Json, Router};
+use route::create_router;
+use tower_http::cors::CorsLayer;
 
-#[tokio::main]
-async fn main() -> Result<(), sqlx::Error> {
-    // initialize tracing for logging
-    tracing_subscriber::fmt().with_max_level(Level::INFO).init();
-    
-    dotenv().ok();
-    let url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    let pool = PgPoolOptions::new().connect(&url).await?;
-    info!("Connected to the database!");
-    
-    // build our application with a route
-    let app = Router::new()
-    // `GET /` goes to `root`
-    .route("/", get(root))
-    //Extension Layer
-    .layer(Extension(pool));
+use sqlx::{postgres::PgPoolOptions, Pool, Postgres};
 
-// run our app with hyper, listening globally on port 5000
-let listener = tokio::net::TcpListener::bind("0.0.0.0:5000").await.unwrap();
-info!("Server is running on http://0.0.0.0:5000");
-axum::serve(listener, app).await.unwrap();
-
-Ok(())
+pub struct AppState {
+    db: Pool<Postgres>,
 }
 
-// handler for GET /
-async fn root() -> &'static str {
-    "Hello, world!"
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+
+    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let pool = match PgPoolOptions::new()
+        .max_connections(10)
+        .connect(&database_url)
+        .await
+    {
+        Ok(pool) => {
+            println!("✅Connection to the database is successful!");
+            pool
+        }
+        Err(err) => {
+            println!("🔥 Failed to connect to the database: {:?}", err);
+            std::process::exit(1);
+        }
+    };
+
+    let cors = CorsLayer::new()
+        .allow_origin("http://localhost:3000".parse::<HeaderValue>().unwrap())
+        .allow_methods([Method::GET, Method::POST, Method::PATCH, Method::DELETE])
+        .allow_credentials(true)
+        .allow_headers([AUTHORIZATION, ACCEPT, CONTENT_TYPE]);
+
+    let app = create_router(Arc::new(AppState { db: pool.clone() })).layer(cors);
+
+    println!("🚀 Server started successfully");
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
